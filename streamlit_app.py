43e6125ac33cceb16f379198d432041a476845bf
Thilo Reich <45871693+T3hlo@users.noreply.github.com>
2020-12-09 08:11:48 +0000

    Initial commit

---
+++ streamlit_app.py
@@ -0,0 +1,46 @@
+from collections import namedtuple
+import altair as alt
+import math
+import numpy as np
+import pandas as pd
+import streamlit as st
+
+"""
+# Welcome to Streamlit!
+
+Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+
+If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
+forums](https://discuss.streamlit.io).
+
+In the meantime, below is an example of what you can do with just a few lines of code:
+"""
+
+
+with st.echo(code_location='below'):
+    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
+    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+
+    Point = namedtuple('Point', 'x y')
+    data = []
+
+          # Compute x^2 + y^2 across a 2D grid
+    x, y = np.meshgrid(range(-5, 5), range(-5, 5))
+    z = x ** 2 + y ** 2
+
+    # Convert this grid to columnar data expected by Altair
+    source = pd.DataFrame({'x': x.ravel(),
+                         'y': y.ravel(),
+                         'z': z.ravel()})
+
+    st.altair_chart(alt.Chart(source).mark_rect().encode(
+    x='x:O',
+    y='y:O',
+    color='z:Q'
+)
+        
+        )
+
+    
+
+
