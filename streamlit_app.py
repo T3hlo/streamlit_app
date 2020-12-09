36d0335cbca59b0f7dfa2e935caeeebb391c9f9b
Thilo Reich <45871693+T3hlo@users.noreply.github.com>
2020-12-09 08:35:58 +0000

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,45 +20,44 @@ In the meantime, below is an example of what you can do with just a few lines of
 col1, col2 = st.beta_columns(2)
 
 
-with col1:
-    st.echo(code_location='below'):
-        total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-        num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+with st.echo(code_location='below'):
+    
+        with col:
+            
+            total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
+            num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
 
-        Point = namedtuple('Point', 'x y')
-        data = []
+            Point = namedtuple('Point', 'x y')
+            data = []
 
-              # Compute x^2 + y^2 across a 2D grid
-        x, y = np.meshgrid(range(-5, 5), range(-5, 5))
-        z = x ** 2 + y ** 2
+                  # Compute x^2 + y^2 across a 2D grid
+            x, y = np.meshgrid(range(-5, 5), range(-5, 5))
+            z = x ** 2 + y ** 2
 
-        # Convert this grid to columnar data expected by Altair
-        source = pd.DataFrame({'x': x.ravel(),
-                             'y': y.ravel(),
-                             'z': z.ravel()})
+            # Convert this grid to columnar data expected by Altair
+            source = pd.DataFrame({'x': x.ravel(),
+                                 'y': y.ravel(),
+                                 'z': z.ravel()})
 
-        st.altair_chart(alt.Chart(source).mark_rect().encode(
-        x='x:O',
-        y='y:O',
-        color='z:Q'
-    ), 
+            st.altair_chart(alt.Chart(source).mark_rect().encode(
+            x='x:O',
+            y='y:O',
+            color='z:Q'
+        ), 
 
-                        alt.Chart(source).mark_rect().encode(
-        x='x:O',
-        y='y:O',
-        color='z:Q'
-    )
+                            alt.Chart(source).mark_rect().encode(
+            x='x:O',
+            y='y:O',
+            color='z:Q'
+        )
 
-            )
+                )
 
 
 
 
 
 with col2:
-
-    st.echo(code_location='below'):
-
 
         # Compute x^2 + y^2 across a 2D grid
         x, y = np.meshgrid(range(-5, 5), range(-5, 5))
