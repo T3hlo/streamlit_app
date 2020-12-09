36c593c3c6e076e03ca7f943b6515ea42c01c3bb
Thilo Reich <45871693+T3hlo@users.noreply.github.com>
2020-12-09 09:12:43 +0000

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,35 +25,25 @@ This is some _markdown_.
 '''
 
 with st.echo(code_location='below'):
-    
     total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
     num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
 
     Point = namedtuple('Point', 'x y')
     data = []
 
-                      # Compute x^2 + y^2 across a 2D grid
-    x, y = np.meshgrid(range(-5, 5), range(-5, 5))
-    z = x ** 2 + y ** 2
+    points_per_turn = total_points / num_turns
 
-                # Convert this grid to columnar data expected by Altair
-    source = pd.DataFrame({'x': x.ravel(),
-                                     'y': y.ravel(),
-                                     'z': z.ravel()})
+    for curr_point_num in range(total_points):
+        curr_turn, i = divmod(curr_point_num, points_per_turn)
+        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
+        radius = curr_point_num / total_points
+        x = radius * math.cos(angle)
+        y = radius * math.sin(angle)
+        data.append(Point(x, y))
 
-    st.altair_chart(alt.Chart(source).mark_rect().encode(
-                x='x:O',
-                y='y:O',
-                color='z:Q'
-            ), 
-
-    alt.Chart(source).mark_rect().encode(
-                x='x:O',
-                y='y:O',
-                color='z:Q'
-            )
-
-                    )
+    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
+        .mark_circle(color='#0068c9', opacity=0.5)
+        .encode(x='x:Q', y='y:Q'))
 
 '''
 This is an interatcive plot.
