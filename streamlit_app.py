20cbf2e7a237d202f5bff85eea213f692a07dd2c
Thilo Reich <45871693+T3hlo@users.noreply.github.com>
2020-12-09 08:36:18 +0000

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,7 @@ col1, col2 = st.beta_columns(2)
 
 with st.echo(code_location='below'):
     
-        with col:
+        with col1:
             
             total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
             num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
