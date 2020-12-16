74830a0b4306268a21d67d7074acfd022c4bd241
Thilo Reich <45871693+T3hlo@users.noreply.github.com>
2020-12-16 15:18:52 +0000

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,8 +26,10 @@ chart_data = pd.read_csv('AVPU.csv')
 chart_data.date = pd.to_datetime(chart_data.date)
 chart_data = chart_data.set_index('date')
 
-# temp = chart_data['Alert']
+temp = chart_data['Voice']
+st.line_chart(chart_data)
 
+temp = chart_data['Unresponsive']
 st.line_chart(chart_data)
 
 
